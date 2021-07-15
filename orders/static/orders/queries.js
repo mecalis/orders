6bf8cb2c06728f57f546fc06de32ec8023e87f6a
Tamas <mecalis@gmail.com>
2021-07-15 13:02:45 +0200

    Clipboard kész

--- orders/static/orders/queries.js
+++ orders/static/orders/queries.js
@@ -3,12 +3,24 @@ console.log("múkodj7!")
 
 show_order_btn.addEventListener('click', ()=>{
     console.log('bent a lisenerben')
+    var clipboard = new ClipboardJS('.show-meals', {
+        text: function() {
 
-    d = $('#masolni').focus().select();
-//    var copyText = document.querySelector("js-copytextarea");
-//    copyText.focus();
-//    copyText.select();
-    console.log(d)
+//            var $copytext = document.getElementById("modal-to-copy").innerHTML.replace("<p>","");
+            var $copytext = document.getElementById("modal-to-copy").innerText;
+			return $copytext; // document.getElementById(savedID).innerHTML;
+
+        }
+    });
+
+    clipboard.on('success', function(e) {
+        console.log(e);
+    });
+
+    clipboard.on('error', function(e) {
+        console.log(e);
+    });
+
 
     try {
         var successful = document.execCommand('copy');
@@ -20,3 +32,20 @@ show_order_btn.addEventListener('click', ()=>{
     }
 
 })
+
+var clipboard = new ClipboardJS('.menu-item_copy', {
+        text: function() {
+
+            var $copytext = "TESZT";
+			return $copytext; // document.getElementById(savedID).innerHTML;
+
+        }
+    });
+
+    clipboard.on('success', function(e) {
+        console.log(e);
+    });
+
+    clipboard.on('error', function(e) {
+        console.log(e);
+    });
